208155c7c27c93d23f09b2d87cfbc4a21a1455f1
DharaniShilpa2908 <133484125+DharaniShilpa2908@users.noreply.github.com>
2023-09-21 16:13:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let colorPickerContainerElement = document.getElementById("colorPickerContainer");
+let selectedColorHexCodeElement = document.getElementById("selectedColorHexCode");
+
+function backGroundColor1() {
+    colorPickerContainerElement.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCodeElement.textContent = "#e0e0e0";
+}
+
+function backGroundColor2() {
+    colorPickerContainerElement.style.backgroundColor = "#6fcf97";
+    selectedColorHexCodeElement.textContent = "#6fcf97";
+}
+
+function backGroundColor3() {
+    colorPickerContainerElement.style.backgroundColor = "#56ccf2";
+    selectedColorHexCodeElement.textContent = "#56ccf2";
+}
+
+function backGroundColor4() {
+    colorPickerContainerElement.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCodeElement.textContent = "#bb6bd9";
+}
